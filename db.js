887f5add27574b5d2e7a90b2425a4a91582a5863
yousefomar724 <yousefomar724@gmail.com>
2024-08-19 01:48:19 +0300

    remove commented code

--- db.js
+++ db.js
@@ -1,22 +1,3 @@
-// const mysql = require('mysql');
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'Noor1818@',
-//   database: 'pan'
-// });
-
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to the database:', err);
-//     return;
-//   }
-//   console.log('Connected to the MySQL server.');
-// });
-
-// module.exports = connection;
-
 import mongoose from "mongoose";
 
 export const connectDB = (url) => mongoose.connect(url);
